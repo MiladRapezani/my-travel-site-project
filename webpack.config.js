afd7ec6a6fdf14581d6b5e525ec141184a52f93c
MiladRapezani <81347614+MiladRapezani@users.noreply.github.com>
2021-04-05 20:44:09 +0430

    resposive codes, add mixins, add responsive images in html for large-hero

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-nested'),
     require('postcss-simple-vars'),
     require('autoprefixer')
