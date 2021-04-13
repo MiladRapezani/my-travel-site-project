b06a638a24ef82b94bb1bd41cb6592dabe6c4df9
MiladRapezani <81347614+MiladRapezani@users.noreply.github.com>
2021-04-13 16:36:29 +0430

    compeleted receal on scroll javascript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
+
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial-item'), 60);
 
 let mobileMenu = new MobileMenu();
