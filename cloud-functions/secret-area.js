e48b3ae77dc0e6a2acacf49c92f6460476ff5dd0
MiladRapezani <81347614+MiladRapezani@users.noreply.github.com>
2021-04-30 15:07:31 +0430

    create cloud-function

---
+++ cloud-functions/secret-area.js
@@ -0,0 +1,13 @@
+// exports.handler = async function(event, context) {
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify({message: "Hello World"})
+//     };
+// }
+
+exports.handler = function(event, context, callback) {
+    callback(null, {
+        statusCode: 200,
+        body: "Welcome to the super secret area"
+    })
+}
