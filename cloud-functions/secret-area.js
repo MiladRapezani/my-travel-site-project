5eac22653086470310412491bdd74593f236ef05
MiladRapezani <81347614+MiladRapezani@users.noreply.github.com>
2021-05-04 09:19:22 +0430

    update secret-area by async function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,15 +1,8 @@
-// exports.handler = async function(event, context) {
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify({message: "Hello World"})
-//     };
-// }
-
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
     let body;
     const secretContent = `
-        <h3>به ناحیه مخفی سایت، خوش آمدید</h3>
-        <p>اینجا می تونیم بهتون بگیم که امروز آسمون <strong>آبی</strong> هستش. پس برید بیرون و ی چرخی بزنید : )</p>
+    <h3>به ناحیه مخفی سایت، خوش آمدید</h3>
+    <p>اینجا می تونیم بهتون بگیم که امروز آسمون <strong>آبی</strong> هستش. پس برید بیرون و ی چرخی بزنید : )</p>
     `
 
     if(event.body) { body = JSON.parse(event.body) }
@@ -25,5 +18,26 @@ exports.handler = function(event, context, callback) {
             statusCode: 401
         })
     }
-
 }
+
+// exports.handler = function(event, context, callback) {
+//     let body;
+//     const secretContent = `
+//     <h3>به ناحیه مخفی سایت، خوش آمدید</h3>
+//     <p>اینجا می تونیم بهتون بگیم که امروز آسمون <strong>آبی</strong> هستش. پس برید بیرون و ی چرخی بزنید : )</p>
+//     `
+
+//     if(event.body) { body = JSON.parse(event.body) }
+//     else { body = {} }
+
+//     if(body.password === 'javascript') {
+//         callback(null, {
+//             statusCode: 200,
+//             body: secretContent
+//         })
+//     } else {
+//         callback(null, {
+//             statusCode: 401
+//         })
+//     }
+// }
